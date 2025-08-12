b37c6daa57e3608f8571051b5a7b069ebba12ecc
MedivhAran <2064698586@qq.com>
2025-08-12 22:02:37 +0800

    medivh Linux medivh-VMware-Virtual-Platform 6.14.0-27-generic #27-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:01:58 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:02:37 up  4:35,  3 users,  load average: 0.13, 0.35, 0.23 af06bdde9a85abebfec3e2cda0f83cabc30c1956

--- lab5/trans.c
+++ lab5/trans.c
@@ -22,7 +22,7 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    for (int i = 0; i < N; i+= 8)
+      for (int i = 0; i < N; i+= 8)
         for (int j = 0; j < M; j+= 8)
         {
             for (int ii = i; ii < i + 8; ii++)
@@ -54,6 +54,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             B[j][i] = tmp;
         }
     }    
+
 }
 
 /*
